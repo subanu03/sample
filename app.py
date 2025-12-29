5622da2ee5d12deb71358cc8a3971145128269f3
subanu03 <smounttech97@gmail.com>
2025-12-29 14:24:49 +0530

    Remove extra git clone stage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python + Jenkins + Docker 🚀"
+    return "Hello from Python + Jenkins + Docker  hlo world 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
